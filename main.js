c3ebb836085990b8b4152cdb2c6e6ef713f1e4e2
ProfessorGabriel123 <126835277+ProfessorGabriel123@users.noreply.github.com>
2023-09-18 02:37:16 +0000

    tecla som

--- main.js
+++ main.js
@@ -1,8 +1,18 @@
 //nossa função tem que armazernar os comando de som 
-function tocaSomPom(){
+function tocaSomPom() {
 document.querySelector('#som_tecla_pom').play() 
-
 }
+document.querySelector('.tecla_pom').onclick=tocaSomPom;
+//AO pesquisar o botão com o document.querySelector('.tecla_pom') e clicar nele, .onclick, ele vai puchar a referencia tocaSomPom que é a função criada inicialmente
+function tocaSomClap() {
+    document.querySelector('#som_tecla_clap').play()
+}
+document.querySelector('.tecla_clap').onclick=tocaSomClap;
+
+
+
+
+
 // Ao usar as duas varras voce esta comentando
 document.querySelector('.tecla_pom')
 //Para vazer uma pesquisa no querySelector é necessario dizer em qual local esta sendo pesquisado o elemento, por isso utilizamos o document. antes do querySelector
