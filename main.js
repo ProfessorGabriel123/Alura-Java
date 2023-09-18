a9a23450d2ed10009e572d2f4a7478453a4da6f4
ProfessorGabriel123 <126835277+ProfessorGabriel123@users.noreply.github.com>
2023-09-18 03:04:02 +0000

    Lista criada

--- main.js
+++ main.js
@@ -2,20 +2,18 @@
 function tocaSomPom() {
 document.querySelector('#som_tecla_pom').play() 
 }
-document.querySelector('.tecla_pom').onclick=tocaSomPom;
 //AO pesquisar o botão com o document.querySelector('.tecla_pom') e clicar nele, .onclick, ele vai puchar a referencia tocaSomPom que é a função criada inicialmente
 function tocaSomClap() {
     document.querySelector('#som_tecla_clap').play()
 }
-document.querySelector('.tecla_clap').onclick=tocaSomClap;
-
-
-
-
-
 // Ao usar as duas varras voce esta comentando
-document.querySelector('.tecla_pom')
-//Para vazer uma pesquisa no querySelector é necessario dizer em qual local esta sendo pesquisado o elemento, por isso utilizamos o document. antes do querySelector
+document.querySelector('.tecla_pom');
+//document. serve para encontrar o arquivo
 //Se for uma Teg coloque o nome da teg direto EX document.querySelector('h1')
 //Se for o nome da Classe coloque o ponto junto EX document.querySelector('.tecla_pom')
 //Se for um Id utilise o # Ex document.querySelector('# tecla_pom')
+document.querySelectorAll('.tecla');
+/*Este comando pesquesa todos os elementos com a classe tecla*/
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+listaDeTeclas[0].onclick=tocaSomPom;
