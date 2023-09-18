4e112bb95050c46251f1a7fed1ea650c0bbbaf42
ProfessorGabriel123 <126835277+ProfessorGabriel123@users.noreply.github.com>
2023-09-18 02:10:57 +0000

    asdasd

--- main.js
+++ main.js
@@ -3,4 +3,6 @@ document.querySelector('.tecla_pom')
 //Para vazer uma pesquisa no querySelector é necessario dizer em qual local esta sendo pesquisado o elemento, por isso utilizamos o document. antes do querySelector
 //Se for uma Teg coloque o nome da teg direto EX document.querySelector('h1')
 //Se for o nome da Classe coloque o ponto junto EX document.querySelector('.tecla_pom')
-//Se for um Id utilise o # Ex document.querySelector('')
+//Se for um Id utilise o # Ex document.querySelector('# tecla_pom')
+
+document.querySelector('#som_tecla_pom').play()
