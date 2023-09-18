b7c9afd2e71b7a79ba47614f078158ca47d4fd1a
ProfessorGabriel123 <126835277+ProfessorGabriel123@users.noreply.github.com>
2023-09-18 02:02:35 +0000

    querySelector

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
-alert('Ola mundo');
+// Ao usar as duas varras voce esta comentando
+
+document.querySelector('.tecla_pom')
+//Para vazer uma pesquisa no querySelector é necessario dizer em qual local esta sendo pesquisado o elemento, por isso utilizamos o document. antes do querySelector
+//Se for uma Teg coloque o nome da teg direto EX document.querySelector('h1')
+//Se for o nome da Classe coloque o ponto junto EX document.querySelector('.tecla_pom')
+//Se for um Id utilise o # Ex document.querySelector('')
