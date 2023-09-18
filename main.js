ce5ddda36eb47c35497fb7884410a45c5d58dc04
ProfessorGabriel123 <126835277+ProfessorGabriel123@users.noreply.github.com>
2023-09-18 02:10:51 +0000

    Play

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 // Ao usar as duas varras voce esta comentando
-
 document.querySelector('.tecla_pom')
 //Para vazer uma pesquisa no querySelector é necessario dizer em qual local esta sendo pesquisado o elemento, por isso utilizamos o document. antes do querySelector
 //Se for uma Teg coloque o nome da teg direto EX document.querySelector('h1')
